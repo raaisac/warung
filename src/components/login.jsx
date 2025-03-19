98e987d9c07b2afe1e69f5e367d2abfdead8894a
raaisac <raisasacharissa@gmail.com>
2025-03-19 13:06:55 +0700

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { auth, db } from "./firebase";
+import { auth, db } from "./Firebase";
 import { loginUser } from "../utils/Auth";
 import { useNavigate } from "react-router-dom";
 import { getFirestore } from "firebase/firestore";
