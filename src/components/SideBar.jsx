e1fad81a4ede4633270c87f3f5b258eef6479a58
raaisac <raisasacharissa@gmail.com>
2025-03-19 14:17:02 +0700

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ import ProductCarousel from "./ProductCarousel";
 import EditMakan from "./EditMakan";
 import UserAccountTable from "./UserAccountTable";
 import { signOut } from "firebase/auth";
-import { auth } from "../utils/Firebase";
+import { auth } from "../Firebase";
 
 export default function ResponsiveSidebar() {
   const [isOpen, setIsOpen] = useState(false);
