6b3848482be8b3b13bfa59de54396cdae523c5bc
raaisac <raisasacharissa@gmail.com>
2025-03-19 14:16:42 +0700

    Update SignUpPage.jsx

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from "../utils/Firebase";
+import { auth, db } from "../firebase";
 import { setDoc, doc } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
